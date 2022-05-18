a7c9799b020a6e2a36f81c254e545eb563fde1ba
gibson2 <gibsonguillermoarturo@gmail.com>
2022-05-18 00:20:29 -0400

    Did Test, fix dependent of 1-N models in has_many

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     validates_presence_of :title #Validando si titulo esta presente    
     has_rich_text :content #rails action_text:install (Setting of Textbox)
-    has_many :comments #Configuración para conectar el modelo relacional de 1-N entre Post-Comment
+    #Configuración para conectar el modelo relacional de 1-N entre Post-Comment
+    has_many :comments, dependent: :destroy #Solventa dependencia de foring_key cuand ose ejecuta rails test
 end
