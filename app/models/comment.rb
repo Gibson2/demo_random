e7e706cd431be80aa884aecbab215776ff7d985e
gibson2 <gibsonguillermoarturo@gmail.com>
2022-05-18 11:36:34 -0400

    Used turbo_stream in Show page, It is used to reload information in live time

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :post #Configuración para conectar el modelo relacional de 1-N entre Post-Comment
+  broadcasts_to :post #Configuración para la actualización de la pagina de forma inmediata
 end
